051dc2b74adcfafc0f12655aacd4834dd677c7ab
amuso3712-arch <amuso3712@gmail.com>
2026-08-28 18:36:31 +0500

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'fyzen-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/assets/css/styles.css',
+  '/assets/js/main.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
